1e5ef79eb8774aead4a69983c892285b1605f455
Brian Oryem <brionace@gmail.com>
2025-12-27 23:00:15 +0000

    feat: update layout metadata and enhance home page structure with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import FitText from "../lib/FitText";
 import Logo from "./components/logo";
 import Image from "next/image";
@@ -5,24 +6,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen max-h-screen flex flex-col bg-zinc-50 font-sans dark:bg-black">
-      <header className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <FitText
           as={"h1"}
           className="font-sans font-bold tracking-tight text-black dark:text-white"
         >
           Shot Circuit
         </FitText>
-        <div className="h-fit justify-between flex flex-col md:flex-row gap-2 px-4 text-sm text-zinc-600 dark:text-zinc-400">
+        <div className="h-fit justify-between flex flex-row gap-2 px-4 text-sm text-zinc-600 dark:text-zinc-400">
           <p>14A Market Row, Brixton</p>
-          <div className="flex flex-row gap-1">
+          <div className="flex flex-col gap-1">
             <p>Mon-Sun</p>
-            <span>&mdash;</span>
             <time>12pm - 11:30pm</time>
           </div>
         </div>
-      </header>
-      <main className="grow overflow-auto flex flex-col md:flex-row items-center justify-around bg-purple-300 dark:bg-purple-600 bg-linear-to-r from-violet-200 to-pink-200 dark:from-violet-400 dark:to-pink-400  text-center text-2xl font-semibold">
-        <p>Coming Soon!</p>
+      </div>
+      <div className="grow overflow-auto flex flex-col md:flex-row items-center justify-around text-center text-2xl font-semibold">
+        <p className="bg-linear-to-r from-amber-400 to-orange-500 bg-clip-text text-transparent">
+          Coming Soon!
+        </p>
         {/* <div className="flex flex-col items-center grow h-full w-full px-4 py-8">
           <img
             alt=""
@@ -47,16 +49,18 @@ export default function Home() {
           />
           Desserts
         </div> */}
-      </main>
-      <nav className="flex items-center justify-between gap-8 h-auto p-4 border-t border-zinc-200 dark:border-zinc-700">
+      </div>
+      <div className="flex items-center justify-between gap-8 h-auto p-4 border-t border-zinc-200 dark:border-zinc-700">
         <div className="flex justify-around gap-4 lowercase text-zinc-600 dark:text-zinc-400">
-          <a>menu</a>
-          <a>about us</a>
+          <Link href="#">menu</Link>
+          <Link href="#">about us</Link>
         </div>
         <div className="w-8">
-          <Logo />
+          <Link href="/">
+            <Logo />
+          </Link>
         </div>
-      </nav>
+      </div>
     </div>
   );
 }
